64b659510921d1ec84182bf340e9c3e8f2cb7cf1
hanyugeon <hanyugeon@naver.com>
2021-12-24 20:58:21 +0900

    feat: 사용자 숫자 받아오기 기능 추가

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import { disableForm } from "./utils/paint-utils.js";
 import { getRandomNumArr } from "./utils/numbers-utils.js";
 
 const $resultForm = $('game-result-form');
+const $inputNum = $('user-input');
+const $submitBtn = $('submit');
 const randomNumbers = [];
+
+function playGame(e) {
+  e.preventDefault();
+  const userNumbers = $inputNum.value;
+}
 
 function init() {
   disableForm($resultForm);
   getRandomNumArr(randomNumbers);
+  $inputNum.addEventListener('submit', playGame);
+  $submitBtn.addEventListener('click', playGame);
 }
 
 init();
